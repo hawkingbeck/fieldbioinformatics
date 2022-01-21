552f0a05f24aca4f7ae852300570d37bd29c465e
Sam Proctor <sam.proctor@proctor-analytics.com>
2022-01-21 09:55:43 +0000

    Update run pangolin script

--- testPangolin.sh
+++ testPangolin.sh
@@ -1,6 +1,6 @@
 
-export INPUT_DIR=/home/ubuntu/test_data/PGIM21-R085
-export OUTPUT_DIR=./testData
+export INPUT_DIR=/app
+export OUTPUT_DIR=/testData
 
-docker run -e INPUT_DIR -e OUTPUT_DIR paho/pangolin:latest --mount type=bind,source=/home/ubuntu/test_data/PGIM21-R085,target=/home/ubuntu/test_data/PGIM21-R085
+docker run -e INPUT_DIR -e OUTPUT_DIR paho/pangolin:latest --mount type=bind,source=/home/ubuntu/test_data/PGIM21-R085,target=/app
 
