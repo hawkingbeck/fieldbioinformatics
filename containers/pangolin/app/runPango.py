902e860c4559ff18c85e63c8943a40dfa69960f8
Sam Proctor <sam.proctor@proctor-analytics.com>
2022-01-21 09:04:02 +0000

    Add image for pangolin

---
+++ containers/pangolin/app/runPango.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+import sys
+import shutil
+import pandas as pd
+import argparse
+import numpy as np
+from datetime import datetime
+from decimal import Decimal
+
+inputDir = os.getenv('INPUT_DIR')
+outputDir = os.getenv('OUTPUT_DIR')
+
+print(f"Input Dir: {inputDir}")
+print(f"Output Dir {outputDir}")
+
+print(f"Pangolin D Version")
+command = ["pangolin", "-dv"]
+subprocess.run(command)
+
+print(f"Pangolin Version")
+command = ["pangolin", "-v"]
+subprocess.run(command)
+
+print(f"PangoLearn Version")
+command = ["pangolin", "-pv"]
+subprocess.run(command)
+
---
+++ containers/pangolin/buildImage.sh
@@ -0,0 +1,1 @@
+docker build --tag paho/pangolin:latest .
